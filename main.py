d5ae95dc2323deac292ee53898a5fda3d1be6cd9
Kotikf <76152485+Kotikf@users.noreply.github.com>
2023-10-15 12:02:13 +0300

    shutdown_function_implemented main.py
    the pc shutdown function is implemented

--- main.py
+++ main.py
@@ -11,33 +11,23 @@ flag = 0
 
 
 def power_off():
-    with open('ATTENTION!!!', 'a', encoding='utf-8') as file:
-        file.write(f'попытка входы в систему {date.today()} в {datetime.now().strftime("%H:%M:%S")}\n')
-    return os.system('shutdown /s /t 0') 
+    with open('ATTENTION!!!.txt', 'a', encoding='utf-8') as file:
+        file.write(f'Попытка входа в систему {date.today()}  {datetime.now().strftime("%H:%M:%S")}\n')
+    return os.system('shutdown /s /t 0')
 
-    return print('power_off')
+
     
 
-def count_time(delay: int = 15):
-    t = 0
-    while True:
-        t += 5
-        time.sleep(5)
-        if t > 5:
-            power_off()
+import datetime
+import keyboard
 
+def check_state(delay: int, cur_time=datetime.datetime.now()) -> None:
+    new_time = cur_time + datetime.timedelta(seconds=delay)
 
+    while datetime.datetime.now() < new_time:
+        if keyboard.is_pressed('ctrl + alt + d'):
+            break
+    else:
+        power_off()
 
-def wait_click():
-
-    keyboard.wait('ctrl + alt + d')
-    # while True:
-
-    #     keyboard.add_hotkey("ctrl+alt+j", power_off(), count_time())
-
-    #     if flag == 1:
-    #         break
-
-if __name__ == '__main__':
-    Thread(target = count_time()).start()
-    Thread(target = wait_click()).start()
+check_state(10)
